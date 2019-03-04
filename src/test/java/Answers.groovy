42d89885439152017d3d4eb703018cff17fa0c35
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-03-04 22:37:50 +0100

    draft

---
+++ src/test/java/Answers.groovy
@@ -0,0 +1,9 @@
+import spock.lang.Specification
+
+
+/**
+ * Created by mtumilowicz on 2019-03-04.
+ */
+class Answers extends Specification {
+
+}
---
+++ src/test/java/Workshop.groovy
@@ -0,0 +1,9 @@
+import spock.lang.Specification
+
+
+/**
+ * Created by mtumilowicz on 2019-03-04.
+ */
+class Workshop extends Specification {
+
+}
